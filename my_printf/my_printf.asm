45c6b6d7ee616928079823470f219e6934404e7e
sanya-padalko <apadalko90@gmail.com>
2026-03-26 16:22:14 +0300

    Add jump-table
    Добавлена jump-таблица, изменен .gitignore.
    Также теперь небуквенный спецификатор просто выводится, а неизвстный буквенный выводится с '%'

--- my_printf/my_printf.asm
+++ my_printf/my_printf.asm
@@ -1,22 +1,34 @@
 extern printf
 
-%macro save_char_func 2
-	mov qword [spec_jump_table + %1 * 8], %2
+%macro SAVE_CHAR_FUNC 2
+	lea rax, [rel spec_jump_table]
+	mov rbx, %1
+	mov qword [rax + rbx * 8], %2
+%endmacro
+
+%macro PRINT_OUT 2
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, %1
+	mov rdx, %2
+	syscall
 %endmacro
 section .data
 	hex_begin	db '0x'
 	oct_begin 	db '0q'
 	bin_begin 	db '0b'
 	hex_alp 	db '0123456789abcdef'
+	perc		db '%'
 	number		times 0x21 db '0'
-	digit_bit_size	dd 1
+	mask		dd 0
+	digit_bit_size	dd 0
 	minus 		db '-'
 	dec_dig_cnt 	db 0
 	loop_cnt 	db 0
 
 section .data
 spec_jump_table:
-	times 256 dq wrong_spec
+	times 26 dq wrong_spec
 
 section .text
 	global my_printf
@@ -89,9 +101,18 @@ my_printf_cdecl:
 
 .percent:
 	inc r12
-	xor rdx, rdx
-	mov dl, [r12]				; заменить на movzx
-	call qword [spec_jump_table + rdx * 8]
+	movzx rdx, byte [r12]
+	cmp dl, 'a'
+	jl .unknown
+	cmp dl, 'z'
+	jg .unknown
+	lea rax, [rel spec_jump_table]
+	call qword [rax + (rdx - 'a') * 8]
+	jmp .loop
+
+.unknown:
+	PRINT_OUT r12, 1
+	inc r12
 	jmp .loop
 
 .printf_end:
@@ -110,7 +131,11 @@ my_printf_cdecl:
 ;======================================================
 ; Выводит строку с помощью библиотечного printf
 ;
+; Entry:	-
+;
 ; Expected:	rbp + 8 * 3 - 1-й аргумент
+;
+; Destruction:	rax, rbx, rdi, rsi, rdx, rcx, r8, r9, r13, r14
 ;======================================================
 std_printf:
 	mov rdi, [rbp + 8 * 3]			; в rdi форматная строка
@@ -124,14 +149,14 @@ std_printf:
 	mov r8,  [rbx + 8 * 3]			; 5-й
 	mov r9,  [rbx + 8 * 4]			; 6-й
 	add rbx, 8 * 4
-	xor r15, r15
+	xor r14, r14
 
 .save_arg:
 	cmp r13, 0
 	jle .call_printf
 
 	push qword [rbx + r13]
-	inc r15
+	inc r14
 
 	sub r13, 8
 	jmp .save_arg
@@ -141,10 +166,10 @@ std_printf:
 	call printf
 
 .clear_stack:
-	cmp r15, 0
+	cmp r14, 0
 	je .end_clear
 	pop rax
-	dec r15
+	dec r14
 	jmp .clear_stack
 
 .end_clear:
@@ -154,37 +179,58 @@ std_printf:
 
 ;======================================================
 ; Заполняем jump-table
+;
+; Destruction:	rax
 ;======================================================
 jump_table_init:
-	save_char_func '%', print_percent
-	save_char_func 'c', print_char
-	save_char_func 'x', wrap_print_hex
-	save_char_func 'o', wrap_print_oct
-	save_char_func 'b', wrap_print_bin
-	save_char_func 'd', print_dec
-	save_char_func 's', print_string
+	SAVE_CHAR_FUNC 'c' - 'a', print_char
+	SAVE_CHAR_FUNC 'x' - 'a', wrap_print_hex
+	SAVE_CHAR_FUNC 'o' - 'a', wrap_print_oct
+	SAVE_CHAR_FUNC 'b' - 'a', wrap_print_bin
+	SAVE_CHAR_FUNC 'd' - 'a', print_dec
+	SAVE_CHAR_FUNC 's' - 'a', print_string
 	ret
-;------------------------------------------------------
+;-----------------------------------------------------
 
 
-;======================================================
-; Неверный спецификатор
-;======================================================
+;=====================================================
+; В случае неправильного спецификатора ничего не выводим
+;
+; Entry:	-
+;
+; Result:	r12 = r12 + 1
+;
+; Expected:	-
+;
+; Destruction:	r12
+;=====================================================
 wrong_spec:
+	mov rax, 1
+	mov rdi, 1
+	lea rbx, [rel perc]
+	mov rsi, rbx
+	mov rdx, 1
+	syscall
+
+	PRINT_OUT r12, 1
+
 	inc r12
 	ret
 ;------------------------------------------------------
 
 
-;======================================================
-; Вывод '%'
-;======================================================
+;=====================================================
+; Entry:	[r12] = '%'
+;
+; Result:	Выведет символ '%' на экран
+;		r12 = r12 + 1
+;
+; Expected:	-
+;
+; Destruction:	r12, rax, rsi, rdi, rdx
+;=====================================================
 print_percent:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, r12		; [r12] = второй '%'
-	mov rdx, 1
-	syscall
+	PRINT_OUT r12, 1
 
 	inc r12
 	ret
@@ -192,7 +238,16 @@ print_percent:
 
 
 ;=====================================================
-; Вывод %c
+; Entry:	[r13] = ascii-код символа, который нужно вывести
+;
+; Result:	Выведет символ на экран (если он попадает 
+;		в границы от 0 до 255)
+;		r13 = r13 + 8
+;		r12 = r12 + 1
+;
+; Expected:	-
+;
+; Destruction:	r13, r12, rax, rsi, rdi, rdx
 ;=====================================================
 print_char:
 	mov rax, [r13]
@@ -203,11 +258,7 @@ print_char:
 	ret
 
 .right_char:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, r13		; [r13] = переданный char
-	mov rdx, 1
-	syscall
+	PRINT_OUT r13, 1
 
 	add r13, 8
 	inc r12
@@ -216,40 +267,68 @@ print_char:
 
 
 ;=====================================================
-; print_hex трамплин
+; трамплин для вывода шестнадцатеричного числа
+;
+; Result:	устанавливает mask = 4
+;		запускает вывод числа
 ;=====================================================
 wrap_print_hex:
-	mov byte [digit_bit_size], 4
+	lea rax, [rel mask]
+	mov byte [rax], 0xf
+	lea rax, [rel digit_bit_size]
+	mov byte [rax], 4
 	call print_bin
 	ret
 ;-----------------------------------------------------
 
 
 ;=====================================================
-; print_oct-трамплин
+; трамплин для вывода восьмеричного числа
+;
+; Result:	устанавливает mask = 3
+;		запускает вывод числа
 ;=====================================================
 wrap_print_oct:
-	mov byte [digit_bit_size], 3
+	lea rax, [rel mask]
+	mov byte [rax], 0x7
+	lea rax, [rel digit_bit_size]
+	mov byte [rax], 3
 	call print_bin
 	ret
 ;-----------------------------------------------------
 
 
 ;=====================================================
-; print_bin-трамплин
+; трамплин для вывода двоичного числа
+;
+; Result:	устанавливает mask = 1
+;		запускает вывод числа
 ;=====================================================
 wrap_print_bin:
-	mov byte [digit_bit_size], 1
+	lea rax, [rel mask]
+	mov byte [rax], 0x1
+	lea rax, [rel digit_bit_size]
+	mov byte [rax], 1
 	call print_bin
 	ret
 ;-----------------------------------------------------
 
 
 ;=====================================================
-; Вывод числа в системе счисления, равной степени двойки
+; Entry:	[r13] = 32-битное число, которое нужно вывести
+;		в системе счисления, являющейся степенью 2
+;
+; Result:	Выведет число на экран
+;		r13 = r13 + 8
+;		r12 = r12 + 1
+;
+; Expected:	2^digit_bit_size = система счисления
+;
+; Destruction:	r13, r12, rax, rbx, rcx, rdx, rsi, rdi
 ;=====================================================
 print_bin:
-	mov rdi, number
+	lea rbx, [rel number]
+	mov rdi, rbx
 	add rdi, 0x20
 	mov rax, [r13]
 	add r13, 8
@@ -258,14 +337,15 @@ print_bin:
 .save_dig:
 	push rax
 	mov cl, 0x40
-	sub cl, byte [digit_bit_size]
-	shl rax, cl
-	shr rax, cl
-	mov bl, byte [hex_alp + rax]
+	lea rbx, [rel mask]		; заменить сдвиги на маску
+	and rax, qword [rbx]
+	lea rbx, [rel hex_alp]
+	mov bl, byte [rbx + rax]
 	mov byte [rdi], bl
 	
 	dec rdi
-	mov cl, byte [digit_bit_size]
+	lea rbx, [rel digit_bit_size]
+	mov cl, byte [rbx]
 	pop rax
 	shr rax, cl
 	inc rdx
@@ -285,16 +365,24 @@ print_bin:
 
 
 ;=====================================================
-; Вывод десятичного числа
+; Entry:	[r13] = 32-битное число, которое нужно вывести
+;
+; Result:	Выведет на экран 32-битное число [r13]
+;		r13 = r13 + 8
+;		r12 = r12 + 1
+;
+; Expected:	-
+;
+; Destruction:	r13, r12, rax, rbx, rsi, rdi, rdx, rcx
 ;=====================================================
 print_dec:
-	xor rax, rax
 	mov eax, [r13]
 	cmp eax, 0
 	jne .not_null
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, number			; если число равно 0 сразу выводим
+	lea rbx, [rel number]
+	mov rsi, rbx			; если число равно 0 сразу выводим
 	mov rdx, 1
 	syscall
 
@@ -307,7 +395,8 @@ print_dec:
 	jg .positive
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, minus
+	lea rbx, [rel minus]
+	mov rsi, rbx
 	mov rdx, 1
 	syscall
 
@@ -315,18 +404,22 @@ print_dec:
 	neg eax
 
 .positive:
-	mov byte [dec_dig_cnt], 0
-	mov rdi, number
+	lea rbx, [rel dec_dig_cnt]
+	mov byte [rbx], 0
+	lea rbx, [rel number]
+	mov rdi, rbx
 	add rdi, 9			; rdi указывает на последнюю цифру
 
 .print_dec_dig:
 	xor rdx, rdx
 	mov rbx, 10
 	div rbx				; rdx = посл. цифра (остаток при делении на 10)
-	mov cl, [hex_alp + rdx]
+	lea rbx, [rel hex_alp]
+	mov cl, [rbx + rdx]
 	mov byte [rdi], cl
 	dec rdi
-	inc byte [dec_dig_cnt]
+	lea rbx, [rel dec_dig_cnt]
+	inc byte [rbx]
 	cmp eax, 0
 	jne .print_dec_dig
 
@@ -334,8 +427,8 @@ print_dec:
 	mov rsi, rdi
 	mov rax, 1
 	mov rdi, 1
-	xor rdx, rdx
-	mov dl, byte [dec_dig_cnt]
+	lea rbx, [rel dec_dig_cnt]
+	movzx rdx, byte [rbx]
 	syscall
 
 	add r13, 8
@@ -345,7 +438,16 @@ print_dec:
 
 
 ;=====================================================
-; Вывод строки
+; Entry:	[r13] = адрес начала строки
+;		[r12] = 's'
+;
+; Result:	Выведет строку на экран
+;		r13 = r13 + 8
+;		r12 = r12 + 1
+;
+; Expected:	-
+;
+; Destruction:	r13, r12, rax, rcx, rdx, rsi, rdi
 ;=====================================================
 print_string:
 	mov rcx, [r13]
--- test.c
+++ test.c
@@ -3,5 +3,5 @@
 extern void my_printf(const char *format, ...);
 
 int main() {
-    my_printf("%%%d\n%d %s %x %d%%%c%b\n", 5111111, -1, "love", 3802, 100, 33, 126);
+    my_printf("Example, %y\n%%%d\n%d %s %x %d%%%c%b\n", 5111111, -1, "love", 3802, 100, 33, 126);
 }
